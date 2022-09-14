76a1827c389ee2b6b79f3ec961bda930ba3b3392
Daniel Navarrete <daniel_navarrete_05@hotmail.com>
2022-09-13 22:58:41 -0500

    algunos cambios

--- Miawsician/Assets/Script/DownloadMusic/MusicDowloand.cs
+++ Miawsician/Assets/Script/DownloadMusic/MusicDowloand.cs
@@ -2,17 +2,17 @@ using System;
 using System.Collections;
 using System.ComponentModel;
 using System.Net;
+using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class MusicDowloand : MonoBehaviour
 {
     public void btnDownload_Click()
-    {
+    {    
         WebClient webClient = new WebClient();
-        //webClient.DownloadFileAsync(new Uri("https://bitmidi.com/uploads/28048.mid"), @"C:\Users\4\Downloads\Borrar\Miawsicians\Miawsician\Assets\StreamingAssets\MoonLight_Sonata.mid");
-        //webClient.DownloadFileAsync(new Uri("https://bitmidi.com/uploads/28048.mid"), @Application.persistentDataPath + "/sonata_no_14_claro_de_luna.mid");
-        webClient.DownloadFile(new Uri("https://bitmidi.com/uploads/28048.mid"), @Application.persistentDataPath + "/sonata_no_14_claro_de_luna.mid");
+        webClient.DownloadFileAsync(new Uri("http://www.musicamidigratis.com/midis/38/descargar.html"), @Application.streamingAssetsPath + "/testeo.mid");
+        //webClient.DownloadFileAsync(new Uri("http://www.musicamidigratis.com/midis/38/descargar.html"), @Application.persistentDataPath + "/sonata_no_14_claro_de_luna.mid");
         Debug.Log("Complete");
         Debug.Log(Application.persistentDataPath);
     }
