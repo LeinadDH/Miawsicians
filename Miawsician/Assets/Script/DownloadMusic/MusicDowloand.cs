024ad4750b726a0354f72acf6d230ff95b85743d
Daniel Navarrete <daniel_navarrete_05@hotmail.com>
2022-09-14 21:10:39 -0500

    Tengo Fe

--- Miawsician/Assets/Script/DownloadMusic/MusicDowloand.cs
+++ Miawsician/Assets/Script/DownloadMusic/MusicDowloand.cs
@@ -9,18 +9,21 @@ public class MusicDowloand : MonoBehaviour
     public void btnDownload_Click()
     {    
         WebClient webClient = new WebClient();
-        webClient.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0)");
-        webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Downloader_DownloadFileCompleted);
-        webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(Downloader_DownloadProgressChanged);
+        //webClient.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0)");
+        //webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Downloader_DownloadFileCompleted);
+        //webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(Downloader_DownloadProgressChanged);
         //webClient.DownloadFileAsync(new Uri("http://www.musicamidigratis.com/midis/38/descargar.html"), @Application.streamingAssetsPath + "/sonata_no_14_claro_de_luna.mid");
-        webClient.DownloadFileAsync(new Uri("http://www.musicamidigratis.com/midis/38/descargar.html"), @Application.persistentDataPath + "/sonata_no_14_claro_de_luna.mid");
+        webClient.DownloadFileAsync(new Uri("http://www.piano-midi.de/midis/beethoven/mond_3.mid"), @Application.persistentDataPath + "/sonata_no_14_claro_de_luna.mid");
+        /*
         while (webClient.IsBusy) 
         {
             Debug.Log("Complete");
             Debug.Log(Application.persistentDataPath);
         }
+        */
     }
 
+    /*
     private void Downloader_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
     {
         // print progress of download.
@@ -35,4 +38,5 @@ public class MusicDowloand : MonoBehaviour
         else
             Console.WriteLine("Download Completed!!!");
     }
+    */
 }
