18f4cfbe250f9f6f338e89d6cdee72e7e8a3c149
Daniel Navarrete <daniel_navarrete_05@hotmail.com>
2022-09-14 20:35:30 -0500

    Esperemos funcione

--- Miawsician/Assets/Script/DownloadMusic/MusicDowloand.cs
+++ Miawsician/Assets/Script/DownloadMusic/MusicDowloand.cs
@@ -1,41 +1,38 @@
 using System;
-using System.Collections;
 using System.ComponentModel;
 using System.Net;
-using UnityEditor.PackageManager;
 using UnityEngine;
-using UnityEngine.Networking;
+
 
 public class MusicDowloand : MonoBehaviour
 {
-    public GameObject Canvas;
-
-    private void Start()
-    {
-        Canvas.SetActive(true);
-    }
-
     public void btnDownload_Click()
     {    
         WebClient webClient = new WebClient();
-        webClient.DownloadFileAsync(new Uri("http://www.musicamidigratis.com/midis/38/descargar.html"), @Application.streamingAssetsPath + "/testeoDos.mid");
-        //webClient.DownloadFileAsync(new Uri("http://www.musicamidigratis.com/midis/38/descargar.html"), @Application.persistentDataPath + "/sonata_no_14_claro_de_luna.mid");
-        Debug.Log("Complete");
-        Debug.Log(Application.persistentDataPath);
-    }
-
-    public void DownloadFile()
-    {
-        WebClient client = new WebClient();
-        client.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadFileCompleted);
-        client.DownloadFile(new Uri("http://www.musicamidigratis.com/midis/38/descargar.html"), @Application.streamingAssetsPath  + "/TesteoTres.mid");
-    }
-
-    void DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
-    {
-        if (e.Error == null)
+        webClient.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0)");
+        webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Downloader_DownloadFileCompleted);
+        webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(Downloader_DownloadProgressChanged);
+        //webClient.DownloadFileAsync(new Uri("http://www.musicamidigratis.com/midis/38/descargar.html"), @Application.streamingAssetsPath + "/sonata_no_14_claro_de_luna.mid");
+        webClient.DownloadFileAsync(new Uri("http://www.musicamidigratis.com/midis/38/descargar.html"), @Application.persistentDataPath + "/sonata_no_14_claro_de_luna.mid");
+        while (webClient.IsBusy) 
         {
-            Canvas.SetActive(false);
+            Debug.Log("Complete");
+            Debug.Log(Application.persistentDataPath);
         }
+    }
+
+    private void Downloader_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+    {
+        // print progress of download.
+        Console.WriteLine(e.BytesReceived + " " + e.ProgressPercentage);
+    }
+
+    private void Downloader_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+    {
+        // display completion status.
+        if (e.Error != null)
+            Console.WriteLine(e.Error.Message);
+        else
+            Console.WriteLine("Download Completed!!!");
     }
 }
