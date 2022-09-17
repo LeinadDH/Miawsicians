d4f76f9e655174d901adabf166f2e8c4c9094b31
Daniel Navarrete <daniel_navarrete_05@hotmail.com>
2022-09-17 17:58:21 -0500

    Más cambios

--- Miawsician/Assets/Script/DownloadMusic/MusicDowloand.cs
+++ Miawsician/Assets/Script/DownloadMusic/MusicDowloand.cs
@@ -9,21 +9,18 @@ public class MusicDowloand : MonoBehaviour
     public void btnDownload_Click()
     {    
         WebClient webClient = new WebClient();
-        //webClient.Headers.Add("User-Agent", "Mozilla/4.0 (compatible; MSIE 8.0)");
-        //webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Downloader_DownloadFileCompleted);
-        //webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(Downloader_DownloadProgressChanged);
+        webClient.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+        webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Downloader_DownloadFileCompleted);
+        webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(Downloader_DownloadProgressChanged);
         //webClient.DownloadFileAsync(new Uri("http://www.musicamidigratis.com/midis/38/descargar.html"), @Application.streamingAssetsPath + "/sonata_no_14_claro_de_luna.mid");
         webClient.DownloadFileAsync(new Uri("http://www.piano-midi.de/midis/beethoven/mond_3.mid"), @Application.persistentDataPath + "/sonata_no_14_claro_de_luna.mid");
-        /*
         while (webClient.IsBusy) 
         {
             Debug.Log("Complete");
             Debug.Log(Application.persistentDataPath);
         }
-        */
     }
 
-    /*
     private void Downloader_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
     {
         // print progress of download.
@@ -38,5 +35,4 @@ public class MusicDowloand : MonoBehaviour
         else
             Console.WriteLine("Download Completed!!!");
     }
-    */
 }
